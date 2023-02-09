6a739867590f5a18546662bf82a0c86ff2b69f2f
Nitesh Kumar <niteshman30@gmail.com>
2023-02-09 11:34:10 +0530

    firstCommit

--- app/src/main/java/com/example/mirrorechat/Application.kt
+++ app/src/main/java/com/example/mirrorechat/Application.kt
@@ -1,23 +1,86 @@
 package com.example.mirrorechat
 
 import android.app.Application
+import android.util.Log
+
 import com.contus.flycommons.LogMessage
 
 
 import com.contusflysdk.ChatSDK
+import com.mirrorflyuikitsdk.MirrorFlyUIKit
+import com.mirrorflyuikitsdk.adapter.MirrorFlyUIKitAdapter
+import com.mirrorflyuikitsdk.interfaces.InitResultHandler
 
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         // For chat logging
-        LogMessage.enableDebugLogging(BuildConfig.DEBUG)
+//        LogMessage.enableDebugLogging(BuildConfig.DEBUG)
+//
+//        ChatSDK.Builder()
+//            .setDomainBaseUrl(BuildConfig.SDK_BASE_URL)
+//            .setLicenseKey(BuildConfig.LICENSE)
+//            .setIsTrialLicenceKey(true)
+//            .build()
+//
+        MirrorFlyUIKit.initFlySDK(applicationContext,object : MirrorFlyUIKitAdapter {
 
-        ChatSDK.Builder()
-            .setDomainBaseUrl(BuildConfig.SDK_BASE_URL)
-            .setLicenseKey(BuildConfig.LICENSE)
-            .setIsTrialLicenceKey(true)
-            .build()
+            override fun setAppName(): String {
+                return "Chat"
+            }
+            override fun setBaseUrl(): String {
+                return "https://api-preprod-sandbox.mirrorfly.com/api/v1/"
+            }
+            override fun setLicence(): String {
+                return "CfIstf8qENtH1M15GKrJxsBp867Zza"
+            }
+            override fun isCallEnabled(): Boolean {
+                return false
+            }
+
+            override fun isContactEnable(): Boolean {
+               return true
+            }
+
+            override fun isGroupEnable(): Boolean {
+                return true
+            }
+
+            override fun isOtherProfileEnable(): Boolean {
+                return true
+            }
+
+            override fun isOwnProfileEnable(): Boolean {
+                return true
+            }
+
+            override fun isTrialLicenceKey(): Boolean {
+                return true
+            }
+        })
+
+        MirrorFlyUIKit.defaultThemeMode = MirrorFlyUIKit.ThemeMode.Light
+
+
+        MirrorFlyUIKit.initUser("USER_IDENTIFIER", object : InitResultHandler {
+
+            override fun onInitResponse(isSuccess: Boolean, e: String) {
+                if (isSuccess) {
+                    Log.d("niteshResponse", "onInitResponse called with: isSuccess = $isSuccess")
+                } else {
+                    Log.e("niteshResponse", "onInitResponse called with: Failure, e = $e")
+                }
+            }
+        })
+
+
+
+
 
     }
-}
+
+
+
+
+    }
--- app/src/main/java/com/example/mirrorechat/MainActivity.kt
+++ app/src/main/java/com/example/mirrorechat/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mirrorechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.SyncStateContract.Constants
@@ -13,74 +14,79 @@ import com.contusflysdk.activities.FlyBaseActivity
 import com.contusflysdk.api.*
 import com.contusflysdk.api.models.ChatMessage
 import com.example.mirrorechat.databinding.ActivityMainBinding
+import com.mirrorflyuikitsdk.activities.FlyRecentChatActivity
 import org.json.JSONObject
 
-class MainActivity : FlyBaseActivity() {
-    lateinit var binding: ActivityMainBinding
+class MainActivity : FlyRecentChatActivity() {
+    //lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding=DataBindingUtil. setContentView(this,R.layout.activity_main)
-
-        binding.send.setOnClickListener {
-            sendMessages()
-        }
-
-        FlyCore.registerUser("1232165") { isSuccess, throwable, data ->
-            if(isSuccess) {
-                val responseObject = data["data"] as JSONObject
-                // Get Username and password from the object
-                Log.d("nk/response","register succes")
-                Log.d("nk/response",responseObject.getString("username")+responseObject.getString("password"))
-                connectUser(responseObject.getString("username"))
-            } else {
-                Log.d("nk/response",data.toString())
-                // Register user failed print throwable
-                // to find the exception details.
-            }
-            fun connectUser(userId:String){
-
-
-            }
-        }
-
-    }
-
-    private fun sendMessages() {
-        FlyMessenger.sendTextMessage("12321@xmpp-preprod-sandbox.mirrorfly.com", "hii", listener = object :
-            SendMessageListener {
-            override fun onResponse(isSuccess: Boolean, chatMessage: ChatMessage?) {
-                // you will get the message sent success response
-                Log.d("nk/response",isSuccess.toString())
-            }
-        })
+//       binding=DataBindingUtil. setContentView(this,R.layout.activity_main)
+//
+//        binding.send.setOnClickListener {
+//            sendMessages()
+//        }
+//
+//        FlyCore.registerUser("1278") { isSuccess, throwable, data ->
+//            if(isSuccess) {
+//                val responseObject = data["data"] as JSONObject
+//                // Get Username and password from the object
+//                Log.d("nk/response","register succes")
+//                Log.d("nk/response",responseObject.getString("username")+responseObject.getString("password"))
+//                connectUser(responseObject.getString("username"))
+//            } else {
+//                val intent=Intent(this@MainActivity,ChatActivity::class.java)
+//                startActivity(intent)
+//                Log.d("nk/response",data.toString())
+//                // Register user failed print throwable
+//                // to find the exception details.
+//            }
+//            fun connectUser(userId:String){
+//
+//
+//            }
+//        }
 
     }
 
-    private fun connectUser(string: String) {
-        ChatManager.connect(object : ChatConnectionListener {
-            override fun onConnected() {
-                val userJid=FlyUtils.getJid(string)
-                Log.d("nk/response",userJid)
-                binding.send.text=userJid
-                // Write your success logic here to navigate Profile Page or
-                // To Start your one-one chat with your friends
-            }
-
-            override fun onDisconnected() {
-                // Connection disconnected
-                // No need implementations
-            }
-
-            override fun onConnectionNotAuthorized() {
-                // Connection Not authorized
-                // No need implementations
-            }
-        })
-    }
-
-    override fun onMessageReceived(message: ChatMessage) {
-        super.onMessageReceived(message)
-        Log.d("nk/response","received")
-        Toast.makeText(this, message.getMessageTextContent(), Toast.LENGTH_SHORT).show()
-    }
+//    private fun sendMessages() {
+//        FlyMessenger.sendTextMessage("1278@xmpp-preprod-sandbox.mirrorfly.com", "hii", listener = object :
+//            SendMessageListener {
+//            override fun onResponse(isSuccess: Boolean, chatMessage: ChatMessage?) {
+//                // you will get the message sent success response
+//                Log.d("nk/response",isSuccess.toString())
+//            }
+//        })
+//
+//    }
+//
+//    private fun connectUser(string: String) {
+//        ChatManager.connect(object : ChatConnectionListener {
+//            override fun onConnected() {
+//                val userJid=FlyUtils.getJid(string)
+//                Log.d("nk/response",userJid)
+//                binding.send.text=userJid
+//                val intent=Intent(this@MainActivity,ChatActivity::class.java)
+//                startActivity(intent)
+//                // Write your success logic here to navigate Profile Page or
+//                // To Start your one-one chat with your friends
+//            }
+//
+//            override fun onDisconnected() {
+//                // Connection disconnected
+//                // No need implementations
+//            }
+//
+//            override fun onConnectionNotAuthorized() {
+//                // Connection Not authorized
+//                // No need implementations
+//            }
+//        })
+//    }
+//
+//    override fun onMessageReceived(message: ChatMessage) {
+//        super.onMessageReceived(message)
+//        Log.d("nk/response","received")
+//        Toast.makeText(this, message.getMessageTextContent(), Toast.LENGTH_SHORT).show()
+//    }
 }
